bdb28b5ac4e09a271768481e0a11a49679e01374
Yubeenpark <50792773+Yubeenpark@users.noreply.github.com>
2024-09-03 12:46:25 +0000

    add put,delete method

--- express-demo/express-demo.js
+++ express-demo/express-demo.js
@@ -25,6 +25,7 @@ app.get('/youtubers',(req,res)=>{
 db.set(1,youtuber1);
 app.use(express.json());
 
+//등록
 app.post('/youtubers',  (req, res)=>{
   //body에 있는 데이터 화면에 보내기
   try{
@@ -40,6 +41,7 @@ app.post('/youtubers',  (req, res)=>{
 
 })
 
+//정보 조회
 app.get('/youtubers/:id', function (req, res) {
   let {id} =req.params;
   id = parseInt(id);
@@ -53,3 +55,51 @@ app.get('/youtubers/:id', function (req, res) {
   }
 })
 
+//id별 유튜버 삭제
+app.delete('/youtubers/:id',(req,res)=>{
+  let {id}  = req.params;
+  id = parseInt(id);
+  youtuber = db.get(id);
+  let msg= "";
+  
+  if (yotuber=== undefined){
+    msg = `요청하신 id:${id}는 없는 id 입니다. `;
+    res.json({message:msg});
+  }
+  else{
+    
+    db.delete(id);
+    msg = `${youtuber.channelTitle}님 다음에 만나요!`;
+    res.json({message:msg});
+  }
+
+//전체 삭제
+app.delete('/youtubers',(req,res)=>{
+  let msg = "";
+  try{
+    if (db.size===0){
+      msg = "삭제할 유튜버가 없습니다.";
+      res.json({message:msg})
+    }
+    else{
+      msg = "전체 유튜버 삭제가 완료되었습니다.";
+      db.clear();
+      res.json({message:msg});
+     
+    } 
+  }
+  catch(err){
+    res.json({message:err}); 
+
+  }
+
+})
+
+//유튜버 수정
+app.put('/youtubers/:id',(req,res)=>{
+
+})
+
+
+
+})
--- express-demo/youtuber-demo.js
+++ express-demo/youtuber-demo.js
@@ -37,6 +37,42 @@ db.set(2,youtuber2);
 db.set(3,youtuber3);
 
 
+
+b.set(1,youtuber1);
+app.use(express.json());
+
+//등록
+app.post('/youtubers',  (req, res)=>{
+  //body에 있는 데이터 화면에 보내기
+  try{
+    db.set(db.size+1,req.body);
+    res.json({
+      message: `${req.body.channelTitle}님, 등록 완료되었습니다. `
+    });
+  }catch(err){
+    res.json({message: `${err} 발생`});
+  }
+
+
+
+})
+
+//정보 조회
+app.get('/youtubers/:id', function (req, res) {
+  let {id} =req.params;
+  id = parseInt(id);
+
+  if (db.get(id)=== undefined){
+    res.json({message:'error'});
+  }
+  else{
+    youtuber = db.get(id);
+    res.json(youtuber);
+  }
+})
+
+
+/*
 app.get('/youtuber/:id', function (req, res) {
     let {id} = req.params;
     id = parseInt(id);
@@ -52,3 +88,98 @@ app.get('/youtuber/:id', function (req, res) {
   
   })
   
+
+ 
+  */
+
+  //전체 유튜버 조회
+
+  app.get('/youtubers',(req,res)=>{
+    const allyoutuber = Object.fromEntries(db);
+    
+    res.json(allyoutuber);
+    
+    /*
+    
+    let yotubers = {};
+    
+    db.forEach((value,key)=>{
+      youtubers[key] = value;
+    });
+    res.json(yotubers);
+    */
+})
+
+
+
+//id별 유튜버 삭제
+app.delete('/youtubers/:id',(req,res)=>{
+  let {id}  = req.params;
+  id = parseInt(id);
+  youtuber = db.get(id);
+  let msg= "";
+  
+  if (yotuber=== undefined){
+    msg = `요청하신 id:${id}는 없는 id 입니다. `;
+    res.json({message:msg});
+  }
+  else{
+    
+    db.delete(id);
+    msg = `${youtuber.channelTitle}님 다음에 만나요!`;
+    res.json({message:msg});
+  }
+
+//전체 삭제
+app.delete('/youtubers',(req,res)=>{
+  let msg = "";
+  try{
+    if (db.size===0){
+      msg = "삭제할 유튜버가 없습니다.";
+      res.json({message:msg})
+    }
+    else{
+      msg = "전체 유튜버 삭제가 완료되었습니다.";
+      db.clear();
+      res.json({message:msg});
+     
+    } 
+  }
+  catch(err){
+    res.json({message:err}); 
+
+  }
+
+})
+
+//유튜버 수정
+app.put('/youtubers/:id',(req,res)=>{
+
+  let {id}  = req.params;
+  id = parseInt(id);
+  youtuber = db.get(id);
+  let msg= "";
+  let newInfo = {
+    channelTitle: req.body.channelTitle,
+    sub : req.body.sub,
+    VideoNum : req.body.videoNum
+  }
+  if (yotuber=== undefined){
+    msg = `요청하신 id:${id}는 없는 id 입니다. 수정이 어렵습니다. `;
+    
+  }
+  else{
+    
+    db.set(id,newInfo);
+    msg = `${youtuber.channelTitle}님 수정된 정보는 다음과 같습니다.
+  title: ${youtuber.channelTitle}, 
+  구독자:${yotuber.sub}, 
+  동영상 갯수:${youtuber.videoNum}
+  `;
+  }
+  res.json({message:msg});
+})
+
+
+
+})
